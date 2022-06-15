4755c273eede524858507611b90439509418a3b2
tunglevt <tungle@vt.edu>
2022-06-15 13:22:50 -0400

    Update session 2

---
+++ Session_2/exercise_1.rs
@@ -0,0 +1,16 @@
+// Exercise 1
+// Mục đích: giải quyết vấn đề ownership and borrowing không dùng clone()
+fn main() {
+    let x = change_value(10, &mut 20);
+    println!("x = {}", x);
+}
+
+fn change_value(input:u32, output: &mut u32) -> u32{
+    if input ==1 {
+        *output = 3;
+    }
+    else {
+        *output = 4;
+    }
+    *output
+}
